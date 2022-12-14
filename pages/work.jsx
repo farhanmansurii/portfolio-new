eb0398ebf43d91b04570c36a16e1de7af4ea7d0a
Farhan <mansurifarhanfm@gmail.com>
2022-12-14 13:26:51 +0530

    d

--- pages/work.jsx
+++ pages/work.jsx
@@ -11,19 +11,19 @@ export default function work() {
     setShowImage(false);
   };
   return (
-    <div className='text-[#bebebe]/20 text-4xl lg:text-7xl duration-300 w-11/12 mx-auto '>
+    <div className='text-[#bebebe]/20 text-4xl lg:text-7xl duration-150 w-11/12 mx-auto '>
       <div  onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave} className='border-b-[1px] hover:bg-[#bebebe] flex hover:text-[#262525]  hover:px-4  duration-300 py-4 '>
+      onMouseLeave={handleMouseLeave} className='border-b-[1px] hover:bg-[#bebebe] flex hover:text-[#262525]  hover:px-4  duration-150 py-4 '>
         <Link href='/workdetail' >
         <div>
         <span className='text-lg'>(01)</span> Portfolio</div> 
         </Link>
         {showImage && ( <img className="absolute w-[100px] lg:w-[300px] right-4" src='https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=799&q=80'/>)}</div>
 
-      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-300 py-4'><span className='text-lg'>(02)</span> E-commerce</div>
-      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-300 py-4'><span className='text-lg'>(03)</span> SpicyAnime</div>    
-      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-300 py-4'><span className='text-lg'>(04)</span> To-do App</div>    
-      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-300 py-4'><span className='text-lg'>(05)</span> Javascript Game</div>    
+      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-150 py-4'><span className='text-lg'>(02)</span> E-commerce</div>
+      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-150 py-4'><span className='text-lg'>(03)</span> SpicyAnime</div>    
+      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-150 py-4'><span className='text-lg'>(04)</span> To-do App</div>    
+      <div className='border-b-[1px] hover:bg-[#bebebe] hover:text-[#262525]  hover:px-4 duration-150 py-4'><span className='text-lg'>(05)</span> Javascript Game</div>    
     </div>
   )
 }
--- pages/workdetail.jsx
+++ pages/workdetail.jsx
@@ -9,7 +9,12 @@ export default function workdetail() {
           <img className="" src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" />
           <div className="text-white   bottom-10 lg:bottom-16 relative ">
             <Link href={'/work'}>
-              <div className="text-md px-4 bg-black/10 backdrop-blur-[2px] rounded-full w-fit">back to (All Projects)</div>
+              <div className="text-md px-4 py-2 mx-3 flex gap-2 bg-black/10 hover:bg-[#262525]/60 duration-150  backdrop-blur-[2px] rounded-full w-fit">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                </svg>
+                ( All Projects )
+              </div>
             </Link>
             <div className=" text-4xl lg:text-7xl">Title of the project</div>
           </div>
@@ -22,8 +27,14 @@ export default function workdetail() {
             creative arts sectors, and have been fortunate to collaborate with some great people.
           </div>
           <div className="flex justify-between  lg:flex my-10">
-            <div className="text-white animate-pulse ">Visit Live Site</div>
-            <div className="bg-white  px-4">Visit Github</div>
+            <div className="text-white animate-pulse flex gap-2 my-auto ">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
+              </svg>
+              Visit Live Site
+            </div>
+            <div className="bg-white py-2 px-4 hover:scale-95 duration-150">Visit Github</div>
           </div>{' '}
           <div>
             <div className="text-white border-b-[1px] my-2  text-3xl border-gray-500 w-full ">TECH STACK</div>
