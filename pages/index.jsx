f2bc829c1e11f5990ac6aaf5881b9b431985787a
Farhan <mansurifarhanfm@gmail.com>
2022-12-14 13:37:20 +0530

    d

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 
 const Home = () => {
   return (
-   <div className="text-[#bebebe]">hi</div>
+   <div className="text-[#bebebe] text-4xl w-[95%] mx-auto py-20" >Hello welcome to the home page</div>
   )
 }
 
