2369c5fa30131ccd790d5de422bfdf1aa81f09e0
Farhan <mansurifarhanfm@gmail.com>
2022-12-17 02:36:52 +0530

    hi

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -39,13 +39,16 @@ export default function workdetail({ data }) {
           <div className="text-xl italic text-[#bebebe] mb-3 underline underline-offset-auto">Description</div>
           <div className="text-[#bebebe]/70">{data.description}</div>
           <div className="flex justify-between  lg:flex my-10">
+              { data.liveLinks !== '-' && 
             <div className="text-[#bebebe] animate-pulse flex gap-2 my-auto ">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z" />
               </svg>
-              <a href={data.liveLinks}>Visit Live Site</a>
+
+                <a href={data.liveLinks}>Visit Live Site</a>
             </div>
+              }
             <div className="bg-[#bebebe] py-2 px-4 hover:scale-95 duration-150">
               <a href={data.github}>Visit Github</a>
             </div>
