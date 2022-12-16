250f6896fb5717709881eb06249b846107de05c4
Farhan <mansurifarhanfm@gmail.com>
2022-12-16 11:59:02 +0530

    dd

--- pages/cv.jsx
+++ pages/cv.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { PulseLoader } from "react-spinners";
 export default function cv() {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div className=" text-[#bebebe] mx-12">
-      <iframe
+    <div className=" text-[#bebebe] ">
+      {loading && (
+          <div className="text-[#bebebe] w-full h-[400px] flex justify-center items-center">
+          <PulseLoader color="#bebebe" className='w-fit'  /></div>
+      )}
+        <iframe
         src="https://drive.google.com/file/d/1mGb3NJ2yQHFOXJl1Q2km0eqE199JaHJQ/preview"
-        className="w-full  aspect-auto h-[500px]"
-        allow="autoplay"
-      ></iframe>
+        className="w-full  aspect-auto h-[400px]"
+        onLoad={() => setLoading(false)}
+        />
     </div>
   );
 }
