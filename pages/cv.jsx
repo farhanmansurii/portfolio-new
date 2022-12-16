e892da1614a54bbac71135c0578f0e642ab9c0a0
Farhan <mansurifarhanfm@gmail.com>
2022-12-17 00:52:57 +0530

    a

--- pages/cv.jsx
+++ pages/cv.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { PulseLoader } from "react-spinners";
+import { CircleSpinner } from "react-spinners-kit";
 export default function cv() {
   const [loading, setLoading] = useState(true);
   return (
     <div className=" text-[#bebebe] ">
       {loading && (
           <div className="text-[#bebebe] w-full h-[400px] flex justify-center items-center">
-          <PulseLoader color="#bebebe" className='w-fit'  /></div>
+          <CircleSpinner size={30} color="#262525" className='w-fit' loading={loading} /></div>
       )}
         <iframe
         src="https://drive.google.com/file/d/1mGb3NJ2yQHFOXJl1Q2km0eqE199JaHJQ/preview"
