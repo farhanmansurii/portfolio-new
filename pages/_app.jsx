cc445f00a31f95d795431a84f35bd68c29577663
Farhan <mansurifarhanfm@gmail.com>
2022-12-19 02:52:30 +0530

    modified about

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,16 @@
+import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-
 function MyApp({ Component, pageProps }) {
   return (
-    <div className='flex flex-col min-h-screen'>
-
-    <Navbar/>
-    <div className="py-[4rem]">
-      <Component {...pageProps} />
-    </div>
-    <Footer/>
+    <div className="flex flex-col min-h-screen">
+      <NextNProgress color="#777777" startPosition={0.3} stopDelayMs={200} height={5} options={{ showSpinner: false }} showOnShallow={true} />
+      <Navbar />
+      <div className="py-[4rem]">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </div>
   );
 }
