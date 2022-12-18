85d01a058d119ef527265cf6e6f1c2402bb89744
Farhan <mansurifarhanfm@gmail.com>
2022-12-19 01:30:08 +0530

    hi

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
-
+import {
+  SiChakraui,
+  SiCss3,
+  SiExpress,
+  SiFirebase,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiJenkins,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiNodemon,
+  SiPython,
+  SiReact,
+  SiRedux,
+  SiTailwindcss
+} from 'react-icons/si';
 export default function Home() {
   return (
     <div className="text-[125px] lg:text-[244.554px] w-[98%] mx-auto leading-none">
@@ -28,10 +45,30 @@ export default function Home() {
           <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">r</div>
         </div>
         <div className="text-2xl  h-fit lg:w-[350px]    mt-4 mx-auto">
-          <span className='mr-3 text-[#323232]'>
-            ABOUT
-            </span>
-             Hi I'm Farhan mansuri ,I am a developer based in Mumbai MH, India focused on creating interactive digital experiences on the web.I build elegant, simple, and intuitive web interfaces that effectively communicate products to their intended users using modern web technologies{' '}
+          <span className="mr-3 text-[#323232]">ABOUT</span>
+          Hi I'm Farhan mansuri ,I am a developer based in Mumbai MH, India focused on creating interactive digital experiences on the web.I build elegant, simple, and intuitive web interfaces that
+          effectively communicate products to their intended users using modern web technologies{' '}
+        </div>
+      </div>
+      <div className='my-10 text-4xl w-[98%] mx-auto'>
+        Skills & Technologies
+        <div className="grid grid-cols-4 mx-auto lg:grid-cols-8 w-fit  gap-9 ">
+          <SiHtml5 className="w-12 h-12" />
+          <SiCss3 className="w-12 h-12" />
+          <SiJavascript className="w-12 h-12" />
+          <SiReact className="w-12 h-12" />
+          <SiRedux className="w-12 h-12" />
+          <SiNextdotjs className="w-12 h-12" />
+          <SiMongodb className="w-12 h-12" />
+          <SiExpress className="w-12 h-12" />
+          <SiNodedotjs className="w-12 h-12" />
+          <SiNodemon className="w-12 h-12" />
+          <SiFirebase className="w-12 h-12" />
+          <SiTailwindcss className="w-12 h-12" />
+          <SiChakraui className="w-12 h-12" />
+          <SiGit className="w-12 h-12" />
+          <SiPython className="w-12 h-12" />
+          <SiJenkins className="w-12 h-12" />
         </div>
       </div>
     </div>
