e368e6b462d7c31981c5df65afdaaa9bf4c4bb61
Farhan <mansurifarhanfm@gmail.com>
2022-12-18 19:26:10 +0530

    hi

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,33 +1,29 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 export default function Navbar() {
   return (
     <div className="fixed w-full bg-[#262525]/40 backdrop-blur-[2px]">
       <div className="text-[#bebebe]   text-lg lg:text-2xl flex justify-between mx-auto w-[95%] pt-3 pb-2 border-b-[2px] capitalize border-[#bebebe]">
         <div>
           <Link href="/">
-            <div className="hover:line-through left-0 block lg:hidden  ease-in-out duration-200   ">
-              FM '98
-            </div>
+            <div className="hover:line-through left-0 block lg:hidden  ease-in-out duration-200   ">FM '98</div>
           </Link>
           <Link href="/">
-            <div className="hover:line-through hidden lg:block  ease-in-out duration-200   ">
-              farhan mansuri '98
-            </div>
+            <div className="hover:line-through hidden lg:block  ease-in-out duration-200   ">farhan mansuri '98</div>
           </Link>
         </div>
 
         <div className="flex  gap-2 lg:gap-4">
-          <Link href="/work">
-            <div className="hover:line-through ease-in-out duration-200   ">
-              Work
-            </div>
+          <Link href="/about">
+            <div className="hover:line-through ease-in-out duration-200   ">about</div>
           </Link>
-         
+
+          <Link href="/work">
+            <div className="hover:line-through ease-in-out duration-200   ">Work</div>
+          </Link>
+
           <Link href="/cv">
-            <div className="hover:line-through ease-in-out duration-200   ">
-              Resume
-            </div>
+            <div className="hover:line-through ease-in-out duration-200   ">Resume</div>
           </Link>
         </div>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,56 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 export default function Home() {
-  const [showButton, setShowButton] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const handleScroll = () => {
-    if (window.pageYOffset > 100) {
-      setShowButton(true);
-    } else {
-      setShowButton(false);
-    }
-  };
-
-  const handleClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
   return (
-    <div onScroll={handleScroll}>
-      <div className=" h-100vh  bg-[#bebebe] text-4xl lg:text-6xl py-4 w-[95%] mx-auto">
-        <div className="  flex flex-col lg:flex-row-reverse">
-          {/* <div className=" w-fit mx-auto my-auto">
-            {loading && (
-              <div className="text-[#bebebe] w-[250px] h-[250px] flex justify-center items-center">
-                <CircleSpinner size={30} color="#262525" loading={loading} />
-              </div>
-            )}
-           <Spline scene="https://prod.spline.design/o9K2aFollWgnrqL9/scene.splinecode" onLoad={() => setLoading(false)} />
-          </div> */}
-
-          <div className=" p-4">
-            I’m a front-end developer. With a focus on methodical code, I enjoy making websites that are innovative, user-focused , designed for me and for people, accessible & performant .
-          </div>
+    <div className="text-[140px] lg:text-[240.554px] leading-none">
+      <div className="flex flex-wrap">
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out   text-bebas">F</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">r</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">o</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">n</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">t</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">—</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out hidden lg:block text-bebas">—</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">e</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">n</div>
+        <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">d</div>
+      </div>
+      <div className="flex  lg:flex-row flex-col">
+        <div className="flex flex-wrap   ">
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">D</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">E</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">v</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">e</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">l</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">o</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">p</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">e</div>
+          <div className=" text-[#777777] hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">r</div>
+        </div>
+        <div className="text-xl text-[#777777] h-fit lg:w-[300px]   mt-4 mx-auto">
+          {' '}
+          <span className='mr-4 text-[#323232]'>
+            ABOUT
+            </span>
+             I am a developer based in Mumbai MH, India focused on creating interactive digital experiences on the web.{' '}
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row gap-5 w-[95%] mx-auto mt-[5rem] ">
-        <div className="w-full text-[#bebebe] lg:text-3xl  px-4 text-xl mx-auto text-end">About me</div>
-        <div className="h-100vh  bg-[#bebebe] lg:text-3xl px-4 py-4  mx-auto">
-          Hi there! My name is Farhan Mansuri and I'm a fresher front-end developer with a passion for Front-End Developement. I also have some experience with back-end development, which gives me a
-          well-rounded understanding of the web development process.
-          <br /> <br /> I recently graduated with a Masters Degree in Computer Science from a S.K Somaiya College, where I learned the skills I need to build user-friendly and visually appealing web
-          applications.
-          <br /> <br /> I also have some experience with back-end development, which gives me a well-rounded understanding of the web development process. Whilst writing code is what I do, it’s my
-          passion & appreciation of the arts . <br /> <br />
-          Over the years I’ve been fortunate enough to study,learn and explore the new technologies , I’m interested in all kinds of music, anime , movies, gaming, football and the great outdoors. I
-          absolutely love animals specially cats.
-          <br /> <br /> I’m always open to hearing about new projects and opportunities so please feel free to get in touch.
-        </div>
-      </div>
-      <button className="fixed bottom-0 right-0 mb-10 lg:mb-11 mr-5 bg-[#262525] rounded-full border-2 border-[#bebebe] px-4 py-2  text-[#bebebe]" onClick={handleClick}>
-        Back to top
-      </button>
     </div>
   );
 }
