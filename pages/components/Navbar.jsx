7f11bd8152bd1fd0992d22d61afaf260e18bd259
farhan <mansurifarhanfm@gmail.com>
2022-12-24 00:57:17 +0530

    CV page loader

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,37 +1,43 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 export default function Navbar() {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
   return (
-    <div className="fixed w-[98%] mx-auto bg-[#111111]/40 backdrop-blur-[2px]">
+    <div className="fixed w-full mx-auto bg-[#111111]/40 backdrop-blur-[2px]">
       <div className="  text-lg lg:text-2xl flex justify-between mx-auto  w-[98%] pt-3 pb-2 border-b-[2px] capitalize border-[#777777]">
         <div>
           <Link href="/">
-            <div className="hover:line-through   ease-in-out duration-200   ">
-              farhan mansuri '98
-            </div>
+            <div>farhan mansuri '98</div>
           </Link>
         </div>
 
         <div className="flex  gap-2 lg:gap-4">
           <Link href="/">
-            <div className="hover:line-through ease-in-out duration-200   ">
+            <div className={currentRoute === "/" ? "line-through" : ""}>
               Home
             </div>
           </Link>
           <Link href="/about">
-            <div className="hover:line-through ease-in-out duration-200   ">
+            <div className={currentRoute === "/about" ? "line-through" : ""}>
               About
             </div>
           </Link>
 
           <Link href="/work">
-            <div className="hover:line-through ease-in-out duration-200   ">
+            <div
+              className={
+                currentRoute === "/work" ? "line-through italic font-thin" : ""
+              }
+            >
               Work
             </div>
           </Link>
 
           <Link href="/cv">
-            <div className="hover:line-through ease-in-out duration-200   ">
+            <div className={currentRoute === "/cv" ? "line-through" : ""}>
               CV
             </div>
           </Link>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import React from 'react';
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
 import {
   SiChakraui,
   SiCss3,
@@ -17,64 +17,107 @@ import {
   SiPython,
   SiReact,
   SiRedux,
-  SiTailwindcss
-} from 'react-icons/si';
+  SiTailwindcss,
+} from "react-icons/si";
 export default function Home() {
   return (
-    <div className="text-[125px] lg:text-[244.554px] hilg:my-0 mx-auto leading-none">
-       <Head>
+    <div className="text-[125px] lg:text-[270.554px]  lg:my-0 mx-auto leading-none">
+      <Head>
         <title>Home | Farhan Mansuri</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className='min-h-80vh justify-around'>
+      <div className="min-h-80vh z-0 my-5 lg:my-2 w-full justify-around">
         <div className="flex flex-wrap tracking-tighter ">
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out   text-bebas">F</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">r</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">o</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">n</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">t</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">—</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out text-bebas">—</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">e</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">n</div>
-          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">d</div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out   text-bebas">
+            F
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            r
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            o
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            n
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            t
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            —
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out text-bebas">
+            —
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            e
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            n
+          </div>
+          <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+            d
+          </div>
         </div>
-        <div className="flex  lg:flex-row flex-col">
+        <div className="flex   lg:flex-row flex-col">
           <div className="flex flex-wrap  tracking-tighter scale-[98%] ">
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">D</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">E</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">v</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">e</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">l</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">o</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">p</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">e</div>
-            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">r</div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              D
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              E
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              v
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              e
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              l
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              o
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              p
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              e
+            </div>
+            <div className="  hover:text-[#bebebe] duration-150 ease-in-out  text-bebas">
+              r
+            </div>
           </div>
           <div className="text-2xl  h-fit lg:w-[350px] w-[95%]    mt-4 mx-auto">
             <span className="mr-3 text-[#323232]">ABOUT</span>
-            Hi I'm Farhan mansuri, a developer based in Mumbai MH, India focused on creating elegant, simple, and intuitive web interfaces. Here you can find{' '}
+            Hi I'm Farhan mansuri, a developer based in Mumbai MH, India focused
+            on creating elegant, simple, and intuitive web interfaces. Here you
+            can find{" "}
             <span className="bg-[#777777] text-[#111111] px-1">
               <Link href="/about">about me</Link>
-            </span>{' '}
-            ,{' '}
+            </span>{" "}
+            ,{" "}
             <span className="bg-[#777777] text-[#111111] px-1">
               <Link href="/work">Works</Link>
-            </span>{' '}
-            and my{' '}
+            </span>{" "}
+            and my{" "}
             <span className="bg-[#777777] text-[#111111] px-1">
               <Link href="/work">CV</Link>
-            </span>{' '}
-            and also{' '}
+            </span>{" "}
+            and also{" "}
             <span className="bg-[#777777] text-[#111111] px-1">
-              {' '}
+              {" "}
               <a href="mailto:mansurifarhanfm@gmail.com">Contact me.</a>
             </span>
           </div>
         </div>
       </div>
       <div className="my-12 text-4xl mx-auto grid justify-center place-content-center  space-y-6">
-        <div className="text-4xl justify-center w-fit  "> Skills & Technology——</div>
+        <div className="text-4xl justify-center w-fit  ">
+          {" "}
+          Skills & Technology——
+        </div>
         <div className="grid grid-cols-4 lg:grid-cols-8 w-fit  gap-9 ">
           <SiHtml5 className="w-12 hover:scale-[120%] duration-200 h-12 hover:text-[#bebebe]" />
           <SiCss3 className="w-12 hover:scale-[120%] duration-200 h-12 hover:text-[#bebebe]" />
