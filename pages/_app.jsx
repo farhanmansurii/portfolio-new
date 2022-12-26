985a3eb2a95050c38ad7b41ea1b8711c5193e126
farhan <mansurifarhanfm@gmail.com>
2022-12-27 01:46:08 +0530

    hi

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,13 @@
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
+
 function MyApp({ Component, pageProps }) {
   return (
     <div className="flex flex-col min-h-screen">
@@ -18,7 +23,16 @@ function MyApp({ Component, pageProps }) {
         options={{ showSpinner: false }}
         showOnShallow={true}
       />
+
       <Navbar />
+      <AnimatedCursor
+        innerSize={15}
+        outerSize={15}
+        color="255, 255 ,255"
+        outerAlpha={0.4}
+        innerScale={0.7}
+        outerScale={5}
+      />
       <Component {...pageProps} />
       <Footer />
     </div>
